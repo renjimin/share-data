7f1bbd24486c6c99a2ccecc877587edd2296d2fe
renjimin <renjim@126.com>
2019-05-23 16:10:08 +0800

    打包去掉.map文件

--- vue.config.js
+++ vue.config.js
@@ -22,10 +22,12 @@ module.exports = {
       .set('_views', resolve('src/views'))
       config.resolve.symlinks(true);
   },
-  // chainWebpack: config => {
-  //   // 修复HMR
-  //   config.resolve.symlinks(true);
-  // },
+  runtimeCompiler: true, // 是否使用包含运行时编译器的 Vue 构建版本
+
+  productionSourceMap: false, // 生产环境的 source map
+
+  parallel: require('os').cpus().length > 1,
+
   devServer: {
     proxy: {
       '/api': {
